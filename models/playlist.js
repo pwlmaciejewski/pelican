273a2d9e58b1eb9130a12c64b22db7cb0be1ff7e
Pawel Maciejewski <fragphace@gmail.com>
2012-08-29 22:11:10 +0200

    Player's next() method

--- models/playlist.js
+++ models/playlist.js
@@ -3,7 +3,16 @@ var SongCollection = require('./songCollection.js');
 
 var Playlist = SongCollection.extend({
 	nowPlaying: function () {
-		return false;
+		if (this.length) {
+			return this.first();
+		} else {
+			return false;
+		}
+	},
+
+	next: function () {
+		this.remove(this.first());
+		return this;
 	}
 });
 
--- tests/models/playlist.js
+++ tests/models/playlist.js
@@ -3,21 +3,34 @@ var Playlist = require('../mocks/playlist.js');
 module.exports = {
   setUp: function (callback) {
     this.playlist = new Playlist();
-    callback();
+    this.playlist.add([
+      // "Totally Enormous Extinct Dinosaurs - \"Garden\": SXSW 2011 Showcasing Artist"
+      { ytId: 'KD1NTfTF21I' },
+      // "Amelie Soundtrack - Yann Tiersen (Original)"
+      { ytId: 'WYGFNjEL7Jw' }
+    ]);
+
+    this.playlist.fetch({
+      complete: function () {
+        callback();        
+      }
+    });
   },
 
   emptyPlaylist: function (test) {
-    test.equal(this.playlist.nowPlaying(), false);
+    var playlist = new Playlist();
+    test.equal(playlist.nowPlaying(), false);
     test.done();
   },
 
-  oneSong: function (test) {
-    this.playlist.add({ ytId: 'KD1NTfTF21I' });
-    this.playlist.fetch({
-      complete: function () {
-        test.equal(this.playlist.nowPlaying().get('title'), "Totally Enormous Extinct Dinosaurs - \"Garden\": SXSW 2011 Showcasing Artist");
-        test.done();
-      }
-    });
+  nowPlaying: function (test) {
+    test.equal(this.playlist.nowPlaying().get('title'), "Totally Enormous Extinct Dinosaurs - \"Garden\": SXSW 2011 Showcasing Artist");
+    test.done();
+  },
+
+  next: function (test) {
+    this.playlist.next();
+    test.equal(this.playlist.nowPlaying().get('title'), "Amelie Soundtrack - Yann Tiersen (Original)");
+    test.done();
   }
 };
