80548ac5e4d628600b7822b7f2243a74504aba85
Pawel Maciejewski <fragphace@gmail.com>
2012-09-01 17:05:19 +0200

    'No songs in queue' communicate

--- public/javascripts/src/view/songThumbCollection.js
+++ public/javascripts/src/view/songThumbCollection.js
@@ -18,12 +18,19 @@ define(['underscore', 'backbone', 'view/songThumb'], function (_, Backbone, Song
       }, this);
     },
 
+    empty: function () {
+      return '<li class="empty">No songs in queue</li>'
+    },
+
     reset: function () {
-      this.$el.html('');
+      this.$el.html(this.empty());
       this.collection.each(this.add.bind(this));
     },
 
     add: function (model) {
+      // Clear "empty" communicate
+      this.$el.find('.empty').remove();
+
       // Create new view and push it to `this.views`
       var view = new SongThumb({
         model: model,
@@ -46,13 +53,18 @@ define(['underscore', 'backbone', 'view/songThumb'], function (_, Backbone, Song
         if (reject) {
           view.$el.fadeOut(function () {
             view.remove();
-          });
+
+            // Show "empty" communicate
+            if (!this.views.length) {
+              this.$el.html(this.empty());
+            }
+          }.bind(this));
         }
 
         return reject;
-      });
+      }, this);
     },
-    
+
     views: []
   });
 });
